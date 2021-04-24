800aaecf2cc4ad42dd45dc501a3a90fe8b6457b1
Gabriela <gabrielasigolo12@gmail.com>
2021-04-24 16:59:09 -0300

    Commit Projeto

---
+++ ProjetoPontoNET/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjetoPontoNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name} ! How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What is your genre?");
+            var genre = Console.ReadLine();
+            var sex = "man";
+            if (genre == "F")
+            {
+                sex = "woman";
+            }
+            Console.WriteLine($"{Environment.NewLine} Hello {name}! You are a {sex} and you are {age} years old");
+            
+        }
+    }
+}
